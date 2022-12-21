15438ff2b702d2c73ca3387e42838ad982c863b0
Soulbound60 <90531442+Soulbound60@users.noreply.github.com>
2022-12-21 21:42:49 +0300

    9am wed

--- app/src/main/java/com/example/tasktimer/View/AddTaskActivity.kt
+++ app/src/main/java/com/example/tasktimer/View/AddTaskActivity.kt
@@ -3,12 +3,12 @@ package com.example.tasktimer.View
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.lifecycle.ViewModelProvider
 import com.example.tasktimer.Model.TaskTable
 import com.example.tasktimer.ViewModel.MainViewModel
 import com.example.tasktimer.databinding.ActivityAddTaskBinding
-import kotlinx.android.synthetic.main.activity_add_task.view.*
-import kotlinx.coroutines.CoroutineScope
+
 
 class AddTaskActivity : AppCompatActivity() {
     private lateinit var binding: ActivityAddTaskBinding
@@ -20,8 +20,6 @@ class AddTaskActivity : AppCompatActivity() {
         binding= ActivityAddTaskBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.apply {
-
-
 
             imgRed.setOnClickListener {
                 imgRed.setBackgroundColor(Color.RED)
@@ -42,9 +40,7 @@ class AddTaskActivity : AppCompatActivity() {
             if (taskName.isNotEmpty()&&taskSubject.isNotEmpty()&&color.length>1){
                 var task = TaskTable(0,taskName,taskSubject,0,color,false)
                 viewModel.addTask(task)
-
-
-
+                Log.d("TAG","$task")
 
             }
 
